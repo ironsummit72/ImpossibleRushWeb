b0b061de37bd051e5bbcfe84413f01e4f9d9f7dc
ironsummit72 <ironsumit72@gmail.com>
2023-10-12 16:24:00 +0530

    rotate square on click

--- scripts/game.js
+++ scripts/game.js
@@ -1,13 +1,19 @@
-const play = document.getElementById("play");
+const gamesquare = document.getElementById("gamesquare");
 const gamesounds = {
   gameover: "../assets/sounds/gameover.mp3",
   drop: "../assets/sounds/drop.mp3",
-  isMuted:false
+  isMuted: false,
 };
+let count = Math.round(Math.random() * 4);
+rotateSquare(count);
 
-play.addEventListener("click", () => {
-  Playsound(gamesounds.isMuted, gamesounds.drop);
-  
+gamesquare.addEventListener("click", () => {
+  count++;
+  rotateSquare(count);
+
+  if (count >= 4) {
+    count = 0;
+  }
 });
 
 function Playsound(ismuted, source) {
@@ -15,4 +21,23 @@ function Playsound(ismuted, source) {
   audio.muted = ismuted;
   audio.src = source;
   audio.play();
+}
+function rotateSquare(rotatevalue) {
+  if (rotatevalue === 1) {
+    gamesquare.style.transform = `rotate(${360}deg)`;
+    gamesquare.className = `gamesquare`;
+  } else if (rotatevalue === 2) {
+    gamesquare.style.transform = `rotate(${90}deg)`;
+    gamesquare.className = `gamesquare-b`;
+  } else if (rotatevalue === 3) {
+    gamesquare.style.transform = `rotate(${180}deg)`;
+    gamesquare.className = `gamesquare-g`;
+  } else if (rotatevalue === 4) {
+    gamesquare.style.transform = `rotate(${270}deg)`;
+    gamesquare.className = `gamesquare-y`;
+  }
+  //1 rotateval=red
+  //2 rotateval=blue
+  //3 rotateval=green
+  //4 rotateval=yellow
 }
